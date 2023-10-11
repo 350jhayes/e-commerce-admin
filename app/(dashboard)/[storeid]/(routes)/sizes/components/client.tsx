70adb1921139a0cce472f1896e21fd4a2c6718de
Jordan <jordanhayes3523@gmail.com>
2023-10-11 12:38:34 -0400

    update changes

--- app/(dashboard)/[storeid]/(routes)/sizes/components/client.tsx
+++ app/(dashboard)/[storeid]/(routes)/sizes/components/client.tsx
@@ -8,13 +8,13 @@ import { Separator } from "@/components/ui/separator"
 
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
-import { BillboardColumn, columns } from "./columns"
+import { columns, SizeColumn } from "./columns"
 
-interface BillboardClientProps {
-    data: BillboardColumn[]
+interface SizesClientProps {
+    data: SizeColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const SizeClient: React.FC<SizesClientProps> = ({
     data
 }) => {
     const router = useRouter()
@@ -24,19 +24,19 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage billboards for your store"
+                    title={`Sizes (${data.length})`}
+                    description="Manage sizes for your store"
                 />
-                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                 <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
-            <Heading title="API" description="API calls for Billboards" />
+            <DataTable searchKey="name" columns={columns} data={data} />
+            <Heading title="API" description="API calls for Sizes" />
             <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId" />
+            <ApiList entityName="sizes" entityIdName="sizeId" />
         </>
     )
 }
